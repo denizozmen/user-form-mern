27ea50e8afe4d9c3138998b5d876366b1481a461
denizozmen <mr.denizozmen@gmail.com>
2020-12-23 19:45:19 +0300

    created express module and MongoDb database

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require("express");
 
 require("dotenv").config();
 
+const dbConnect = require("./models/dbConnect");
+
+const app = express();
+const port = process.env.port || 5000;
+app.listen(port, () => {
+  console.log(`I'm listening on port ${port}`);
+});
+dbConnect();

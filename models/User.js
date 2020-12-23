83ffc79225aa99fe252b8fa7f6ad9835524eafb6
denizozmen <mr.denizozmen@gmail.com>
2020-12-23 20:17:10 +0300

    User model schema added

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "created",
+  },
+  description: {
+    type: String,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedDate: {
+    type: Date,
+  },
+  deletedDate: {
+    type: Date,
+  },
+});
+
+module.exports = User = mongoose.model("User", schema);

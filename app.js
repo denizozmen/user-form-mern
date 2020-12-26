5414762976443b19be00b82851221f6bf9c7235c
denizozmen <mr.denizozmen@gmail.com>
2020-12-26 22:54:08 +0300

    Update Deploy Frontend

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
-const path = require("path");
+const bodyParser = require('body-parser');
 const cors = require("cors");
+const path = require('path')
+const app = express();
+const dbConnect = require("./models/dbConnect");
+
+app.use(bodyParser.json());
+app.use(cors());
 
 require("dotenv").config();
-const dbConnect = require("./models/dbConnect");
+
 const router = require("./routers/router");
 
 //server
-const app = express();
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
@@ -15,17 +21,23 @@ app.listen(port, () => {
 
 //database connect
 dbConnect();
-app.use(cors());
+
 
 app.use(express.json());
 app.use("/api", router);
 
-if (process.env.NODE.ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*"),
-    (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    };
-}
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 
-app.use("/", express.static(path.join(__dirname + "/node_modules")));
+// if (process.env.NODE.ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*"),
+//     (req, res) => {
+//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//     };
+// }
+
+// //Çalışan
+// app.use("/", express.static(path.join(__dirname + "/node_modules")));

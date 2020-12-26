fb7c5191286bafad4da375d5b58270f207ce09b6
denizozmen <mr.denizozmen@gmail.com>
2020-12-26 23:26:15 +0300

    Deployed Completely Frontend & Backend

--- app.js
+++ app.js
@@ -30,13 +30,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-// if (process.env.NODE.ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*"),
-//     (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//     };
-// }
 
-// //Çalışan
-// app.use("/", express.static(path.join(__dirname + "/node_modules")));

900a4a1e66edb2f3f162791785f1f70dc0d7826a
denizozmen <mr.denizozmen@gmail.com>
2020-12-27 15:22:05 +0300

    Added Table & Fetchdata & Logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import rootlogo from "./images/logo_root.gif";
+import "./App.css";
+import Usertable from "./components/Table";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="logoreact">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      </div>
+      <div>
+        <img src={rootlogo} className="" alt="logo" />
+      </div>
+      <Usertable />
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        </p> */
+}
+{
+  /* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </a> */
 }
-
-export default App;
---
+++ client/src/helper/FetchData.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+export const fetchData = async (path) => {
+  const response = await axios.get(`${path}`);
+  return response?.data;
+};
